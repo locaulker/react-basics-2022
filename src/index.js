f6f166ccace3b066803f8b4b15db1356aa388e6b
Lawrence <locaulker@gmail.com>
2022-12-26 06:35:36 -0500

    resume with props

--- src/index.js
+++ src/index.js
@@ -14,25 +14,20 @@ const BookList = () => {
   )
 }
 
+const author = "Jordan Moore"
 const Book = () => {
+  const title = "Interesting Facts For Curious Minds"
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="./images/book-1.jpg"
+        alt="Interesting Facts For Curious Minds"
+      />
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
+      <p>{}</p>
     </article>
   )
-}
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/71m+Qtq+HrL._AC_UY327_FMwebp_QL65_.jpg"
-    alt="Interesting Facts For Curious Minds"
-  />
-)
-const Title = () => <h2>Interesting Facts For Curious Minds</h2>
-const Author = () => {
-  return <h4>Jordan Moore</h4>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
