8a08cb8eeba7545568a0a69799822ef8506144cf
Lawrence O. Caulker <locaulker@gmail.com>
2022-12-26 15:02:00 -0500

    end props

--- src/index.js
+++ src/index.js
@@ -3,29 +3,61 @@ import ReactDOM from "react-dom/client"
 
 import "./index.css"
 
+const firstBook = {
+  author: "Jordan Moore",
+  title: "Interesting Facts For Curious Minds",
+  img: "./images/book-1.jpg",
+}
+
+const secondBook = {
+  author: "James Clear",
+  title: "Atomic Habits",
+  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+}
+
+// const author = "Jordan Moore"
+// const title = "Interesting Facts For Curious Minds"
+// const img = "./images/book-1.jpg"
+
+/*
+// parameters
+const someFunc = (param1, param2) => {
+  console.log(param1, param2)
+}
+// arguments
+someFunc("job", "developer")
+*/
+
+// This is the PARENT component
+// props being PASSED from parent to child component
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   )
 }
 
-const author = "Jordan Moore"
-const Book = () => {
-  const title = "Interesting Facts For Curious Minds"
+// This is the CHILD component
+// props being RECEIVED from parent to child component
+const Book = ({ img, title, author }) => {
+  // console.log(props)
+  // const { img, title, author } = props
+
   return (
     <article className="book">
-      <img
-        src="./images/book-1.jpg"
-        alt="Interesting Facts For Curious Minds"
-      />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author.toUpperCase()}</h4>
-      <p>{}</p>
     </article>
   )
 }
